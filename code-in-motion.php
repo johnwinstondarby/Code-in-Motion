a6dd5399d30bce5ce4c08cae07b6c96ce9b3b98b
Johann Darby <johnwinstondarby@gmail.com>
2026-09-16 16:08:14 -0700

    Make repository root installable as WordPress plugin

---
+++ code-in-motion.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Code in Motion
+ * Description: WordPress host packaging for Code in Motion.
+ * Version: 0.1.0
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ * Author: Localis
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once __DIR__ . '/wordpress/code-in-motion.php';
