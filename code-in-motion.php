231e4da24000ede13e1be7d6084218fb89efaa10
Johann Darby <johnwinstondarby@gmail.com>
2026-09-17 20:36:16 -0700

    R22 complete canonical plugin header metadata

--- code-in-motion.php
+++ code-in-motion.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name: Code in Motion
- * Description: WordPress host packaging for Code in Motion.
+ * Plugin URI: https://github.com/johnwinstondarby/Code-in-Motion
+ * Description: WordPress host, runtime bindings, and controls for Code in Motion experiences.
  * Version: 0.1.0
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: Localis
+ * Author URI: https://localis.services/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
