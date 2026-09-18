a83c8ce4759eb719ea4fabbadbfe2563c2e1891f
Johann Darby <johnwinstondarby@gmail.com>
2026-09-17 20:36:22 -0700

    R22 remove duplicate implementation plugin header

--- wordpress/code-in-motion.php
+++ wordpress/code-in-motion.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * Plugin Name: Code in Motion
- * Description: Minimal WordPress host packaging for Code in Motion checkpoint #1.
- * Version: 0.1.0
- * Requires at least: 6.5
- * Requires PHP: 7.4
- * Author: Localis
- * License: GPLv3
- * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * WordPress implementation for Code in Motion.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
