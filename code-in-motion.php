ab3454b11513fa90762629df76b339374bb923d9
Johann Darby <johnwinstondarby@gmail.com>
2026-09-17 17:29:38 -0700

    R21 declare GPLv3 in code-in-motion.php

--- code-in-motion.php
+++ code-in-motion.php
@@ -6,6 +6,8 @@
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: Localis
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
